bdebbd7d5f49fe7e43c3cd79c7dd1a78a41cc7d4
Gopinath V <57871800+GopinathV-1@users.noreply.github.com>
2021-05-25 19:38:31 +0530

    Add files via upload

---
+++ rcb_batsman_runs.js
@@ -0,0 +1,56 @@
+fetch("./rcb_batsman_runs.json")
+    .then(resp => resp.json())
+    .then(data => {
+        Highcharts.chart('container1', {
+            chart: {
+              type: 'column'
+            },
+            title: {
+              text: 'Total runs scored by RCB batsman in IPL'
+            },
+            subtitle: {
+              text: 'Source:<a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
+            },
+            xAxis: {
+              type: 'category',
+              labels: {
+                rotation: -45,
+                style: {
+                  fontSize: '13px',
+                  fontFamily: 'Verdana, sans-serif'
+                }
+              },
+              title: {
+                text: 'Batsman name'
+              }
+            },
+            yAxis: {
+              min: 0,
+              title: {
+                text: 'Runs scored'
+              }
+            },
+            legend: {
+              enabled: false
+            },
+            tooltip: {
+              pointFormat: 'Total runs : <b>{point.y:.0f}</b>'
+            },
+            series: [{
+              name: 'Team name',
+              data: data.rcb_batsman_runs,
+              dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.0f}', 
+                y: 10, // 10 pixels down from the top
+                style: {
+                  fontSize: '13px',
+                  fontFamily: 'Verdana, sans-serif'
+                }
+              }
+            }]
+          });
+        })
